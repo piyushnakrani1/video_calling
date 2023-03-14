58b4848193484484c973bd2a68712c739fa0474e
piyushnakrani1 <npiyush72@gmail.com>
2023-03-14 21:52:14 +0530

    update 3

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import UserDetail
 
 class UserDetailsSerializer(serializers.ModelSerializer):
+    id = serializers.CharField()
     class Meta:
         model = UserDetail
         fields = ['id', 'username', 'is_active', 'reported']
